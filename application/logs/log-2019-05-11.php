2a791d5255079dfc7178442a99a4203ae95c9f10
Muhammad Shahid <hiamshahid3@gmail.com>
2019-05-12 08:26:09 +0500

    new

---
+++ application/logs/log-2019-05-11.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-11 12:34:12 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) D:\xampp\htdocs\ci\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-05-11 12:34:12 --> Unable to connect to the database
+ERROR - 2019-05-11 13:35:10 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:35:24 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:35:27 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:35:30 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:35:33 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:40:58 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:41:08 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:41:22 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:41:25 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:41:54 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2019-05-11 13:42:07 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2019-05-11 13:42:12 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:42:28 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:42:41 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:42:56 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:43:10 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:43:46 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2019-05-11 13:43:47 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:44:10 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:44:18 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:45:08 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:45:11 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:45:18 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:45:28 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:47:03 --> 404 Page Not Found: ../modules/vendor/controllers//index
+ERROR - 2019-05-11 13:48:07 --> 404 Page Not Found: ../modules/vendor/controllers//index
+ERROR - 2019-05-11 13:48:17 --> 404 Page Not Found: ../modules/vendor/controllers//index
+ERROR - 2019-05-11 13:51:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-11 13:53:45 --> 404 Page Not Found: ../modules/vendor/controllers//index
