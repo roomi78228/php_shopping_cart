945b1d52a8a4ac10785eab2aecdf8e9a2cdd8d32
Muhammad Shahid <hiamshahid3@gmail.com>
2019-05-12 08:36:12 +0500

    config

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'shop',
+	'hostname' => 'remotemysql.com',
+	'username' => 'BS8vj7zSr1',
+	'password' => '1yxnGiffyE',
+	'database' => 'BS8vj7zSr1',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
